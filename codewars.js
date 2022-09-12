57c1aba53b38158c1458950381ac95feabdd5ad7
Nadufa <nadufa18404@gmail.com>
2022-09-12 16:13:32 +0300

    wtf

---
+++ codewars.js
@@ -0,0 +1,23 @@
+// 12.09.2022
+
+//TASK 1
+
+
+// let arr = [1, 1, 3, 1, 1, 1, 1, 1]
+// let first = arr[0]
+// let last = arr[arr.length - 1]
+// let beforeLast = arr[arr.length - 2]
+
+// if (first == last) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] != first) {
+//             console.log(arr[i])
+//         }
+//     }
+// } else {
+//     if (first == beforeLast) {
+//         console.log(last)
+//     } else {
+//         console.log(first)
+//     }
+// }
