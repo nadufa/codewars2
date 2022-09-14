b1c3cef8d9024b7ef04d2bb29569a1405632bfed
Nadufa <nadufa18404@gmail.com>
2022-09-14 16:48:28 +0300

    smth task

--- codewars.js
+++ codewars.js
@@ -135,3 +135,14 @@
 
 // //let result = alphabetPosition(text)
 // console.log(alphabetPosition(text))
+
+//task x
+// function betterThanAverage(classPoints, yourPoints) {
+//     // Your code here
+//     var classAvg = 0;
+//     for (var i = 0; i < classPoints.length; i++){
+//       classAvg += classPoints[i]; 
+//     }
+//     classAvg = classAvg/classPoints.length; 
+//     return yourPoints > classAvg;
+//   }
